31f43926c409cf637a6aeb49cda1f560b22f4e31
Calder Coalson <caldercoalson@gmail.com>
2014-02-06 00:20:43 -0600

    Removed a hack which is no longer necessary with Fish 2

--- Unix/Home/.config/fish/functions/__osx_terminal_title.fish
+++
@@ -1,4 +0,0 @@
-function __on_cd_update_terminal --on-variable PWD --description 'Notify Terminal of directory changes'
-	status --is-command-substitution; and return
-	printf '\033]7;%s\07' (echo file://(hostname -s)$PWD |sed -e 's/ /%20/g')
-end
