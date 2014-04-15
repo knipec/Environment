1f72dcacba15a15c94dea8ff3ee4396c522a206d
knipec <carissaknipe@gmail.com>
2014-04-15 17:56:03 -0500

    Removed Calder's annoying prompt.

--- Unix/Home/.config/fish/functions/fish_prompt.fish
+++
@@ -1,8 +0,0 @@
-function fish_prompt --description 'Display the fish prompt'
-	set COLOR1 (set_color $fish_color_host)
-	set COLOR2 (set_color $fish_color_cwd)
-	set COLOR3 (set_color $fish_color_normal)
-	set HOST (hostname)
-	set CWD (prompt_pwd)
-	echo -n "$COLOR1$USER@$HOST $COLOR2$CWD> $COLOR3"
-end
